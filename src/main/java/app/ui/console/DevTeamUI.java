e20c4b8af52cdb8579b3dfcb9cfc014e856db72d
RitaAriana <1201386@isep.ipp.pt>
2021-04-27 15:19:56 +0100

    Dev Team UI

--- src/main/java/app/ui/console/DevTeamUI.java
+++ src/main/java/app/ui/console/DevTeamUI.java
@@ -14,10 +14,11 @@ public class DevTeamUI implements Runnable{
     {
         System.out.println("\n");
         System.out.printf("Development Team:\n");
-        System.out.printf("\t Student Name 1 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 2 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 3 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 4 - 120XXXX@isep.ipp.pt \n");
+        System.out.printf("\t Manuela Leite - 1200720@isep.ipp.pt \n");
+        System.out.printf("\t Carlos Rodrigues - 1201001@isep.ipp.pt \n");
+        System.out.printf("\t José Pessoa - 1201007@isep.ipp.pt \n");
+        System.out.printf("\t Pedro Rocha - 1201382@isep.ipp.pt \n");
+        System.out.printf("\t Rita Ariana Sobral - 1201386@isep.ipp.pt \n");
         System.out.println("\n");
     }
 }
