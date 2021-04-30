b40233c2ccb52720e1ca4d48dd0592bf71a8c79f
RitaAriana <1201386@isep.ipp.pt>
2021-04-30 16:56:53 +0100

    Beginning of the creation of the TestType class

--- src/main/java/app/domain/model/TestType.java
+++ src/main/java/app/domain/model/TestType.java
@@ -1,7 +1,31 @@
 package app.domain.model;
 
+/**
+ * Represents a type of test.
+ */
+
 public class TestType {
 
+    /**
+     * The test type code.
+     */
+    private int code;
+
+    /**
+     * The description of the test type.
+     */
     private String description;
+
+    /**
+     * The test type collecting method.
+     */
+    private String collectingMethod;
+
+    /**
+     *
+     */
+    public TestType(){
+
+    }
 
 }
