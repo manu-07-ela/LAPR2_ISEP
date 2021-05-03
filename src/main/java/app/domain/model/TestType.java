d18117c066d42969fda21ffc9d340042f8ce5e21
RitaAriana <1201386@isep.ipp.pt>
2021-05-03 19:17:47 +0100

    Beginning of the US9 code

--- src/main/java/app/domain/model/TestType.java
+++ src/main/java/app/domain/model/TestType.java
@@ -1,8 +1,7 @@
 package app.domain.model;
 
-import app.domain.store.TestTypeStore;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,34 +31,53 @@ public class TestType {
     private List<ParameterCategory> listOfParameterCategories;
 
     /**
-     *
-     * @param code
-     * @param description
-     * @param collectingMethod
-     * @param listOfParameterCategories
+     * Build an instance of {@code TestType} by receiving the code, description, collection method and associated parameter categories.
+     * @param code The test type code.
+     * @param description The description of the test type.
+     * @param collectingMethod The test type collecting method.
+     * @param listOfParameterCategories List of parameter categories that the test type has associated.
      */
     public TestType (String code, String description, String collectingMethod, List<ParameterCategory> listOfParameterCategories){
+        checkCodeRules(code);
+        checkDescriptionRules(description);
+        checkcollectingMethodRules(collectingMethod);
+        this.code=code;
+        this.description=description;
+        this.collectingMethod=collectingMethod;
+        this.listOfParameterCategories=listOfParameterCategories;
     }
 
     /**
-     *
-     * @param code
+     * Checks whether the code associated with the type of test we intend to create complies with all business rules.
+     * @param code The test type code.
      */
     private void checkCodeRules (String code) {
+        if (StringUtils.isBlank(code))
+            throw new IllegalArgumentException("Code cannot be blank.");
+        if (description.length() > 5 )
+            throw new IllegalArgumentException("The code must be 5 alphanumeric characters.");
     }
 
     /**
-     *
-     * @param description
+     * Checks whether the description associated with the type of test we intend to create complies with all business rules.
+     * @param description The description of the test type.
      */
     private void checkDescriptionRules (String description) {
+        if (StringUtils.isBlank(description))
+            throw new IllegalArgumentException("Description cannot be blank.");
+        if (description.length() > 15)
+            throw new IllegalArgumentException("The description must be a maximum of 15 characters.");
     }
 
     /**
-     *
-     * @param collectingMethod
+     * Checks whether the collecting Method associated with the type of test we intend to create complies with all business rules.
+     * @param collectingMethod The test type collecting method.
      */
     private void checkcollectingMethodRules (String collectingMethod) {
+        if (StringUtils.isBlank(collectingMethod))
+            throw new IllegalArgumentException("Collecting Method cannot be blank.");
+        if (collectingMethod.length() > 20)
+            throw new IllegalArgumentException("The collection method must be a maximum of 20 characters.");
     }
 
 }
--- src/main/java/app/domain/store/TestTypeStore.java
+++ src/main/java/app/domain/store/TestTypeStore.java
@@ -1,4 +1,60 @@
 package app.domain.store;
 
+import app.domain.model.ParameterCategory;
+import app.domain.model.TestType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The different types of tests that exist in a company.
+ */
 public class TestTypeStore {
+
+    /**
+     * List containing all types of tests that exist in a company.
+     */
+    List<TestType> testTypeList = new ArrayList();
+
+    /**
+     * New test type.
+     * @param code The test type code.
+     * @param description The description of the test type.
+     * @param collectingMethod  The test type collecting method.
+     * @param listOfParameterCategories List of parameter categories that the test type has associated.
+     * @return The test type.
+     */
+    public TestType createTestType(String code, String description, String collectingMethod, List<ParameterCategory> listOfParameterCategories){
+        return new TestType(code,description,collectingMethod,listOfParameterCategories);
+    }
+
+    /**
+     * Global validation of a test type.
+     * @param testType Test Type that we intend to validate.
+     * @return false if the test type already exists or is null. Otherwise, it returns true.
+     */
+    public boolean validateTestType(TestType testType) {
+        if (testType == null)
+            return false;
+        return !this.testTypeList.contains(testType);
+    }
+
+    /**
+     * Adds a new test type to the List.
+     * @param testType
+     */
+    public void addTestType(TestType testType) {
+        testTypeList.add(testType);
+    }
+
+    /**
+     * Save the type of test case it is in a valid state
+     * @param testType The type of test we intend to save
+     * @return true if the test type was saved. Otherwise, false.
+     */
+    public boolean saveParameterCategory(TestType testType) {
+        if (!validateTestType(testType))
+            return false;
+        return this.testTypeList.add(testType);
+    }
 }
