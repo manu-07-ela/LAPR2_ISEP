61ec72050da8c04d44f6729c4ce267aa172567d8
RitaAriana <1201386@isep.ipp.pt>
2021-05-04 22:31:09 +0100

    adding methods to the CreateTestTypeController and TestTypeStore classes

--- src/main/java/app/controller/CreateTestTypeController.java
+++ src/main/java/app/controller/CreateTestTypeController.java
@@ -1,4 +1,16 @@
 package app.controller;
 
+/**
+ *
+ * @author Rita Ariana Sobral <1201386@isep.ipp.pt>
+ */
 public class CreateTestTypeController {
+
+    /**
+     *
+     */
+    public CreateTestTypeController(){
+
+    }
+
 }
