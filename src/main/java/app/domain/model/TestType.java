d2d36d7581ae1c3e02580e42977ed59592376bf7
RitaAriana <1201386@isep.ipp.pt>
2021-05-04 18:11:25 +0100

    adding new methods to the TestType class

--- src/main/java/app/domain/model/TestType.java
+++ src/main/java/app/domain/model/TestType.java
@@ -54,7 +54,7 @@ public class TestType {
     private void checkCodeRules (String code) {
         if (StringUtils.isBlank(code))
             throw new IllegalArgumentException("Code cannot be blank.");
-        if (description.length() > 5 )
+        if (code.length() > 5 )
             throw new IllegalArgumentException("The code must be 5 alphanumeric characters.");
     }
 
@@ -79,5 +79,75 @@ public class TestType {
         if (collectingMethod.length() > 20)
             throw new IllegalArgumentException("The collection method must be a maximum of 20 characters.");
     }
+
+    /**
+     * Get the test type code.
+     * @return The test type code.
+     */
+    public String getCode (){
+        return code;
+    }
+
+    /**
+     * Get the test type description.
+     * @return The test type description.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Get the method of collecting the test type.
+     * @return The test type collecting method.
+     */
+    public String getCollectingMethod() {
+        return collectingMethod;
+    }
+
+    /**
+     * Get the categories of parameters associated with the type of test.
+     * @return List of parameter categories that the test type has associated.
+     */
+    public List<ParameterCategory> getListOfParameterCategories() {
+        return listOfParameterCategories;
+    }
+
+    /**
+     * Compare the type of test with the other object provided.
+     * @param o Object we want to compare with the type of test.
+     * @return true if the received object represents another type of test equivalent to the type of test. Otherwise, it returns false.
+     */
+    @Override
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+
+        if(o == null || this.getClass() != o.getClass()){
+            return false;
+        }
+
+        TestType otherTestType = (TestType) o;
+
+        return this.getCode().equals(otherTestType.getCode());
+    }
+
+    /**
+     * Textual description of the test type.
+     * @return Information about the characteristics of the test type.
+     */
+    @Override
+    public String toString(){
+
+        StringBuilder parameterCategories = new StringBuilder();
+        for (ParameterCategory pc : listOfParameterCategories) {
+            parameterCategories.append(pc);
+            parameterCategories.append("\n");
+        }
+
+        return String.format("Test Type: %s\nCode: %s\nCollecting Method: %s\nParameter Categories:\n%s",description,code,collectingMethod,parameterCategories);
+    }
+
+
 
 }
