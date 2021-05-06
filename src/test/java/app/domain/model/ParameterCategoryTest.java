0e2e6a951e933aa13ca3f8bc5a64b9a1e7b10bac
RitaAriana <1201386@isep.ipp.pt>
2021-05-06 19:59:49 +0100

    implementation of unit tests related to the ParameterCategory class

--- src/test/java/app/domain/model/ParameterCategoryTest.java
+++ src/test/java/app/domain/model/ParameterCategoryTest.java
@@ -1,7 +1,36 @@
 package app.domain.model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ParameterCategoryTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureNullIsNotAllowed() {
+        ParameterCategory pc = new ParameterCategory(null, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureCodeMeetsAC2_1() {
+        ParameterCategory pc = new ParameterCategory("10d5th", "Category");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureCodeMeetsAC2_2() {
+        ParameterCategory pc = new ParameterCategory("10d", "Category");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureCodeMeetsAC2_3() {
+        ParameterCategory pc = new ParameterCategory("10d.6", "Category");
+    }
+
+    @Test
+    public void ensureCodeMeetsAC2_4() {
+        ParameterCategory pc = new ParameterCategory("1f5dc", "Category");
+        Assert.assertNotNull(pc);
+    }
 
 }
