d09cd4fe1a60d78c09753e05dc08e35fb348ce7f
RitaAriana <1201386@isep.ipp.pt>
2021-05-06 19:27:24 +0100

    beginning of the implementation of unit tests

--- src/test/java/app/domain/model/TestTypeTest.java
+++ src/test/java/app/domain/model/TestTypeTest.java
@@ -4,6 +4,9 @@ package app.domain.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestTypeTest {
 
     @Before
@@ -12,9 +15,25 @@ public class TestTypeTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void checkNullsNotAllowed(){
+    public void ensureNullIsNotAllowed(){
         TestType tt = new TestType(null,null,null,null);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureLengthCodeNotAllowedByExcess(){
+        List<ParameterCategory> listPC = new ArrayList();
+        listPC.add(new ParameterCategory("12345","hemogram"));
+        TestType tt = new TestType("1g3d7h","description","method",listPC);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ensureLengthCodeNotAllowedByDisability(){
+        List<ParameterCategory> listPC = new ArrayList();
+        listPC.add(new ParameterCategory("12345","hemogram"));
+        TestType tt = new TestType("1g3","description","method",listPC);
+    }
+
+
 
   
 }
