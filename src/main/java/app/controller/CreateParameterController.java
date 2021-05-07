f810369f6c09a6c86b48f6b883b88af6e7aa9b57
pedroccelestino <1201382@isep.ipp.pt>
2021-05-07 16:32:38 +0100

    Implementing the Create Parameter Controller

--- src/main/java/app/controller/CreateParameterController.java
+++ src/main/java/app/controller/CreateParameterController.java
@@ -1,7 +1,14 @@
 package app.controller;
 
 import app.domain.model.Company;
+import app.domain.model.Parameter;
+import app.domain.model.ParameterCategory;
 import app.domain.store.ParameterCategoryStore;
+import app.domain.store.ParameterStore;
+import app.mappers.ParameterCategoryMapper;
+import app.mappers.dto.ParameterCategoryDto;
+
+import java.util.List;
 
 /**
  * The Create Parameter Controller.
@@ -13,19 +20,43 @@ public class CreateParameterController {
     /**
      *
      */
+    private App app;
+
+    /**
+     *
+     */
     private Company company;
+
+    /**
+     *
+     */
+    private Parameter p;
 
     /**
      *
      */
     private ParameterCategoryStore pcStore;
 
+    /**
+     *
+     */
+    private ParameterStore pStore;
 
     /**
      *
      */
+    private ParameterCategoryMapper pcMapper;
+
+
+
+    /**
+     * Instantiates a new Create Parameter Controller.
+     */
     public CreateParameterController() {
-        this.company=App.getInstance().getCompany();
+        this.app = app.getInstance();
+        this.company = app.getCompany();
+        this.pcStore = company.getParameterCategoryStore();
+        this.pStore = company.getParameterStore();
     }
 
     /**
@@ -34,9 +65,41 @@ public class CreateParameterController {
      */
     public CreateParameterController(Company company) {
         this.company = company;
-        this.pcStore = null;
-
+        this.pcStore=company.getParameterCategoryStore();
+        this.pStore=company.getParameterStore();
     }
+
+    /**
+     * Get a list of objects of type ParameterCategoryDTO
+     * @return list with parameterCategoriesDto
+     */
+    public List<ParameterCategoryDto> getParameterCategories(){
+        List<ParameterCategory> listParameterCategories = pcStore.getParameterCategoryList();
+        return pcMapper.toDto(listParameterCategories);
+    }
+
+    /**
+     * New parameter.
+     * @param code The parameter code.
+     * @param shortName the parameter code.
+     * @param description The description of the parameter.
+     * @param selectedCategory The category of the parameter.
+     * @return false if the parameter already exists or is null. Otherwise, it returns true.
+     */
+    public boolean createParameter(String code, String shortName, String description, String selectedCategory){
+        this.p=pStore.createParameter(code,shortName,description,selectedCategory);
+        return this.pStore.validateParameter(p);
+    }
+
+    /**
+     * Save the parameter if it is in a valid state.
+     * @return true if the parameter was saved. Otherwise, false.
+     */
+    public boolean saveParameter() {
+        return this.pStore.saveParameter(p);
+    }
+
+
 
 
 
