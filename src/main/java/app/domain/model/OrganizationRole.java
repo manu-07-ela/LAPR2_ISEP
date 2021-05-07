8b159038880c75159bcd2547c3759fc9f6c00b58
Manuela Leite <1200720@isep.ipp.pt>
2021-05-07 15:34:20 +0100

    Implementation of RegisterEmployeeController

--- src/main/java/app/domain/model/OrganizationRole.java
+++ src/main/java/app/domain/model/OrganizationRole.java
@@ -1,12 +1,15 @@
 package app.domain.model;
+import app.domain.shared.DefaultOrgRoles;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Locale;
 
 /**
  * Represents a role in organization
  * @author Manuela Leite <1200720@isep.ipp.pt>
  */
 
-public class OrganizationRole {
+public class OrganizationRole implements DefaultOrgRoles {
     /**
      * The designation of a role in organization.
      */
@@ -20,6 +23,7 @@ public class OrganizationRole {
         if (!isValidDesignation()) {
             throw new IllegalArgumentException("Designation must have at maximum 15 charcters");
         }
+        if (!isValidOrganizationRole()) throw new IllegalArgumentException ("This organization role do not exist in Company");
         this.designation = designation;
 
     }
@@ -45,7 +49,22 @@ public class OrganizationRole {
      * Checks whether the designation associated with the organization role we intend to create complies with all business rules.
      * @return true if the designation obeys the rules imposed by the business, false otherwise.
      */
+
     public boolean isValidDesignation(){
         return  designation.length() <= 15 || StringUtils.isBlank(designation);
+    }
+
+    /**
+     * Checks whether the designation associated with the organization role we intend to create exist in Company.
+     * @return true if the organization role exists in company, false otherwise.
+     */
+    public boolean isValidOrganizationRole() {
+        boolean flag = false;
+        for (String orgRoleAux : ORGANIZATIONROLES){
+            if (orgRoleAux.toUpperCase(Locale.ROOT).equals(orgRoleAux) && !flag) {
+                flag = true;
+            }
+        }
+        return flag;
     }
 }
