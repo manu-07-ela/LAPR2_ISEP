b87a66f66c2bdabc72e8b057a1818e22bbddcb76
RitaAriana <1201386@isep.ipp.pt>
2021-05-08 22:19:00 +0100

    unit tests in the ParameterCategoryDto class

--- src/test/java/app/mappers/dto/ParameterCategoryDtoTest.java
+++ src/test/java/app/mappers/dto/ParameterCategoryDtoTest.java
@@ -1,5 +1,7 @@
 package app.mappers.dto;
 
+import app.domain.model.Parameter;
+import app.domain.model.ParameterCategory;
 import app.domain.model.TestType;
 import org.junit.Assert;
 import org.junit.Before;
@@ -26,5 +28,33 @@ public class ParameterCategoryDtoTest {
     public void getName() {
         String result = pcDto.getName();
         Assert.assertEquals("test",result);
+    }
+
+    @Test
+    public void parameterCategoriesDtoEquals(){
+        ParameterCategoryDto pc1 = new ParameterCategoryDto("1f5dc", "Category");
+        ParameterCategoryDto pc2 = new ParameterCategoryDto("1f5dc", "Category");
+        Assert.assertEquals(pc1,pc2);
+    }
+
+    @Test
+    public void parameterCategoriesDtoRefEquals(){
+        ParameterCategoryDto pc1 = new ParameterCategoryDto("1f5ac", "Category");
+        ParameterCategoryDto pc2 = pc1;
+        Assert.assertEquals(pc1,pc2);
+    }
+
+    @Test
+    public void parameterCategoriesDtoNotEqualsNull(){
+        ParameterCategoryDto pc1 = new ParameterCategoryDto("1f5ac", "Category");
+        ParameterCategoryDto pc2 = null;
+        Assert.assertNotEquals(pc1,pc2);
+    }
+
+    @Test
+    public void parameterCategoriesDtoNotEquals(){
+        ParameterCategory pc1 = new ParameterCategory("1f5ac", "Category");
+        ParameterCategoryDto pc2 = new ParameterCategoryDto("12345","test");
+        Assert.assertNotEquals(pc1,pc2);
     }
 }
