ab8b9ecbb4b13c8dccd78fe24f593653f4b0002d
JosePessoa007 <1201007@isep.ipp.pt>
2021-05-08 13:31:23 +0100

    US03- Working on Code from Classes of US03

--- src/main/java/app/controller/CreateClientController.java
+++ src/main/java/app/controller/CreateClientController.java
@@ -1,17 +1,56 @@
 package app.controller;
 
 import app.domain.model.Client;
+import app.domain.model.Company;
 import app.domain.store.ClientStore;
-import app.mappers.ClientMapper;
 import app.mappers.dto.ClientDto;
 
 public class CreateClientController {
-//    public Client createClient(ClientDto clientdto){
-//        return ClientMapper.toModel(clientdto);
-//    }
-//    public Client saveClient(){
-//        ClientStore store = company.getClientStore();
-//        return store.saveClient(cl);
-//    }
+    /**
+     *
+     */
+    private Company company;
+    /**
+     *
+     */
+    private Client cl;
+    /**
+     *
+     */
+    private ClientStore store;
 
+    /**
+     *
+     */
+    public CreateClientController() {
+        this(App.getInstance().getCompany());
+    }
+
+    /**
+     *
+     * @param company
+     */
+    public CreateClientController(Company company) {
+        this.company = company;
+        this.store = company.getClientStore();
+        this.cl = null;
+    }
+
+    /**
+     *
+     * @param cldto
+     * @return
+     */
+    public boolean CreateClient(ClientDto cldto) {
+        this.cl = store.createClient(cldto);
+        return store.validateClient(cl);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean saveClient() {
+        return store.saveClient(cl);
+    }
 }
--- src/main/java/app/domain/store/ClientStore.java
+++ src/main/java/app/domain/store/ClientStore.java
@@ -1,14 +1,65 @@
 package app.domain.store;
 
 import app.domain.model.Client;
+import app.mappers.ClientMapper;
+import app.mappers.dto.ClientDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientStore {
-    public boolean validateClient(Client client){
-        if (client == null) {
-            return false;
-        }
-        return true;
+    /**
+     *
+     */
+    List<Client> clientList;
+
+    /**
+     *
+     */
+    public ClientStore() {
+        clientList = new ArrayList<>();
     }
-//    public Client saveClient(){
-//    }
+
+    /**
+     * @param cldto
+     * @return
+     */
+    public Client createClient(ClientDto cldto) {
+        return ClientMapper.toModel(cldto);
+    }
+
+    /**
+     * @param cl
+     * @return
+     */
+    public boolean validateClient(Client cl) {
+        if (cl == null)
+            return false;
+        return !this.clientList.contains(cl);
+    }
+
+    /**
+     *
+     * @param cl
+     * @return
+     */
+    public boolean addClient(Client cl){
+        return clientList.add(cl);
+    }
+
+    /**
+     * @return
+     */
+    public List<Client> getClientList() {
+        return clientList;
+    }
+
+    public boolean saveClient(Client cl) {
+        if (!validateClient(cl)) {
+            return false;
+        } else {
+            this.clientList.add(cl);
+            return true;
+        }
+    }
 }
