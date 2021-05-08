711b53b5bfc57960ad58f18af9002e6fe901ca08
1201001 <1201001@isep.ipp.pt>
2021-05-09 00:11:43 +0100

    Some changes in the classes related to US08 and unit Tests

--- src/main/java/app/mappers/dto/ClinicalAnalysisLaboratoryDTO.java
+++ src/main/java/app/mappers/dto/ClinicalAnalysisLaboratoryDTO.java
@@ -44,11 +44,6 @@ public class ClinicalAnalysisLaboratoryDTO {
      * @param tin                     Clinical Analysis Laboratory's tax identification number
      */
     public ClinicalAnalysisLaboratoryDTO (String name, String address, String phoneNumber , String tin,String laboratoryId, List<TestTypeDTO> listOfTestTypes){
-        nameValidation(name);
-        AddressValidation(address);
-        phoneNumberValidation(phoneNumber);
-        tinValidation(tin);
-        laboratoryIdValidation(laboratoryId);
         this.name=name;
         this.address=address;
         this.phoneNumber=phoneNumber;
@@ -135,63 +130,4 @@ public class ClinicalAnalysisLaboratoryDTO {
         this.laboratoryId = laboratoryId;
     }
 
-    /**
-     *
-     * @param name   Clinical Analysis Laboratory's name
-     */
-    private void nameValidation(String name){
-        if (StringUtils.isBlank(name)){
-            throw  new IllegalArgumentException("The name mustn't have more than 20 characters");
-        }else if(name.length()>20){
-            throw  new IllegalArgumentException("The name mustn't have more than 20 characters");
-        }
-
-    }
-
-    /**
-     *
-     * @param address     Clinical Analysis Laboratory's address
-     */
-    private void AddressValidation(String address){
-        if (StringUtils.isBlank(address)){
-            throw  new IllegalArgumentException("The address mustn't have more than 30 characters");
-        }else if (address.length()>30){
-            throw  new IllegalArgumentException("The address mustn't have more than 30 characters");
-        }
-    }
-
-    /**
-     *
-     * @param phoneNumber
-     */
-    private void phoneNumberValidation(String phoneNumber){
-        if (StringUtils.isBlank(phoneNumber)){
-            throw  new IllegalArgumentException("Phone number has to have 11 digits");
-        }else if (phoneNumber.length()!=11){
-            throw  new IllegalArgumentException("Phone number has to have 11 digits");
-        }
-    }
-
-    /**
-     *
-     * @param tin
-     */
-    private void tinValidation(String tin){
-        if (StringUtils.isBlank(tin)){
-            throw  new IllegalArgumentException("Tin has to have 10 digits");
-        }else if (tin.length()!=10){
-            throw  new IllegalArgumentException("Tin has to have 10 digits");
-        }
-    }
-
-    /**
-     *
-     * @param laboratoryId Clinical Analysis Laboratory's ID
-     */
-    private void laboratoryIdValidation(String laboratoryId){
-        if (StringUtils.isBlank(laboratoryId))
-            throw  new IllegalArgumentException("The laboratoryId must have only 5 alphanumeric characters");
-        if ( !StringUtils.isAlphanumeric(laboratoryId) || laboratoryId.length() != 5 )
-            throw  new IllegalArgumentException("The laboratoryId must have only 5 alphanumeric characters");
-    }
 }
