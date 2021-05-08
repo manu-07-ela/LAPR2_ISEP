9a59543bf9321d144e016ebf6de1ea2a003d43e2
RitaAriana <1201386@isep.ipp.pt>
2021-05-08 16:14:00 +0100

    unit tests in theTestType class

--- src/test/java/app/domain/model/TestTypeTest.java
+++ src/test/java/app/domain/model/TestTypeTest.java
@@ -1,6 +1,7 @@
 package app.domain.model;
 
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -35,11 +36,6 @@ public class TestTypeTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void ensureCodeMeetsAC1_3(){
-        TestType tt = new TestType("","description","method",listPC);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
     public void ensureDescriptionMeetsAC2(){
         TestType tt = new TestType("1g3d7","descriptionteste","method",listPC);
     }
@@ -47,6 +43,41 @@ public class TestTypeTest {
     @Test(expected = IllegalArgumentException.class)
     public void ensureCollectingMethodMeetsAC3(){
         TestType tt = new TestType("1g354","description","collectingmethodteste",listPC);
+    }
+
+    @Test
+    public void testTypesCodesEquals(){
+        TestType tt1 = new TestType("1f5dc", "test","collecting",listPC);
+        TestType tt2 = new TestType("1f5dc", "teste","collectingmethod",listPC);
+        Assert.assertEquals(tt1,tt2);
+    }
+
+    @Test
+    public void testTypesDescriptionEquals(){
+        TestType tt1 = new TestType("1f5da", "test","collecting",listPC);
+        TestType tt2 = new TestType("1f5dc", "test","collectingmethod",listPC);
+        Assert.assertEquals(tt1,tt2);
+    }
+
+    @Test
+    public void testTypesRefEquals(){
+        TestType tt1 = new TestType("1f5da", "test","collecting",listPC);
+        TestType tt2 = tt1;
+        Assert.assertEquals(tt1,tt2);
+    }
+
+    @Test
+    public void testTypesNotEqualsNull(){
+        TestType tt1 = new TestType("1f5da", "test","collecting",listPC);
+        TestType tt2 = null;
+        Assert.assertNotEquals(tt1,tt2);
+    }
+
+    @Test
+    public void testTypesNotEquals(){
+        TestType tt1 = new TestType("1f5da", "test","collecting",listPC);
+        ParameterCategory pc1 = new ParameterCategory("1f5ac", "Category");
+        Assert.assertNotEquals(pc1,tt1);
     }
 
 
