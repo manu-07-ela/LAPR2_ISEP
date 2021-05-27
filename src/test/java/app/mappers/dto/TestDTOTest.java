3dfbc5c69c457b9fcb499689cd16cf9cf2792dcf
RitaAriana <1201386@isep.ipp.pt>
2021-05-27 14:45:55 +0100

    Unit Test - TestDto

--- src/test/java/app/mappers/dto/TestDTOTest.java
+++ src/test/java/app/mappers/dto/TestDTOTest.java
@@ -9,8 +9,9 @@ public class TestDTOTest {
 
     @Test
     public void getInternalCode() {
-
-
+        TestDTO tDto = new TestDTO("123456789123","Zaragatoa");
+        String result = tDto.getInternalCode();
+        Assert.assertEquals("123456789123",result);
     }
 
 }
