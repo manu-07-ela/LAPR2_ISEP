b290fec71252788ed93d17029ee89cdc3a95a925
JosePessoa007 <1201007@isep.ipp.pt>
2021-05-29 23:03:58 +0100

    Nhscode unitary tests

---
+++ src/test/java/app/mappers/dto/ParameterDTOTest.java
@@ -0,0 +1,21 @@
+package app.mappers.dto;
+
+import junit.framework.TestCase;
+
+public class ParameterDTOTest extends TestCase {
+
+    public void testGetCode() {
+    }
+
+    public void testGetShortName() {
+    }
+
+    public void testGetDescription() {
+    }
+
+    public void testGetCategory() {
+    }
+
+    public void testTestToString() {
+    }
+}
