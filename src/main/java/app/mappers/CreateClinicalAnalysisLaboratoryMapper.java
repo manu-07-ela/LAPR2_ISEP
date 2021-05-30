84d03767e1e78db8aae3488f364afdde95f38fa4
JosePessoa007 <1201007@isep.ipp.pt>
2021-05-30 04:46:57 +0100

    Adding Unitary tests and resolving major bugs

--- src/main/java/app/mappers/CreateClinicalAnalysisLaboratoryMapper.java
+++ src/main/java/app/mappers/CreateClinicalAnalysisLaboratoryMapper.java
@@ -1,10 +1,12 @@
 package app.mappers;
 
 import app.domain.model.laboratories.ClinicalAnalysisLaboratory;
+import app.domain.model.testrelated.Parameter;
 import app.domain.model.testrelated.ParameterCategory;
 import app.domain.model.testrelated.TestType;
 import app.mappers.dto.ClinicalAnalysisLaboratoryDTO;
 import app.mappers.dto.ParameterCategoryDTO;
+import app.mappers.dto.ParameterDTO;
 import app.mappers.dto.TestTypeDTO;
 
 import java.util.ArrayList;
@@ -72,5 +74,73 @@ public class CreateClinicalAnalysisLaboratoryMapper {
         return new ParameterCategory (parameterCategory.getCode(),parameterCategory.getName());
     }
 
+    /**
+     * Transforms the list of ClinicalAnalysisLaboratory into a list of ClinicalAnalysisLaboratoryDTO
+     * @param labs A list of ClinicalAnalysisLaboratory
+     * @return A list of ClinicalAnalysisLaboratoryDTO
+     */
+    public List<ClinicalAnalysisLaboratoryDTO> toDto (List<ClinicalAnalysisLaboratory> labs){
+        List<ClinicalAnalysisLaboratoryDTO> clinicalAnalysisLaboratoryDTO = new ArrayList<>();
+        for(ClinicalAnalysisLaboratory lab: labs) {
+            clinicalAnalysisLaboratoryDTO.add(this.toDto(lab));
+        }
+        return clinicalAnalysisLaboratoryDTO;
+    }
 
+    /**
+     * Transforms an Objects of the type ClinicalAnalysisLaboratory into an Objects of the type ClinicalAnalysisLaboratoryDTO
+     * @param cal an Object of the type ClinicalAnalysisLaboratory
+     * @return an Object of the type ClinicalAnalysisLaboratoryDTO
+     */
+    public ClinicalAnalysisLaboratoryDTO toDto(ClinicalAnalysisLaboratory cal){
+        return new ClinicalAnalysisLaboratoryDTO(cal.getName(),cal.getAddress(), cal.getPhoneNumber(), cal.getTin(), cal.getLaboratoryId(),toDtoo(cal.getListOfTestTypes()));
+    }
+
+    /**
+     * Transforms the list of TestType into a list of TestTypeDTO
+     * @param testTypeList A list of TestTypeDTO
+     * @return A list of TestTypeDTO
+     */
+    public List<TestTypeDTO> toDtoo(List<TestType> testTypeList) {
+        List<TestTypeDTO> testTypeListDTO =new ArrayList();
+        TestTypeDTO obj;
+        for (TestType lista : testTypeList) {
+            if (lista != null){
+                obj = toDtoo(lista);
+                testTypeListDTO.add(obj);
+            }
+        }
+        return testTypeListDTO;
+    }
+
+    /**
+     * Transforms a TestType into a TestTypeDTO
+     * @param testType a TestType
+     * @return a TestTypeDTO
+     */
+    public TestTypeDTO toDtoo (TestType testType){
+        return new TestTypeDTO (testType.getCode(), testType.getDescription(),testType.getCollectingMethod(),toDtopclist(testType.getListOfParameterCategories()), testType.getReferenceAdapter());
+    }
+
+    /**
+     * Transforms a list of objects of type ParameterCategory into a list of objects of type ParameterCategoryDTO.
+     * @param parameterCategories A list of ParameterCategory.
+     * @return A list of ParameterCategoryDTO
+     */
+    public List<ParameterCategoryDTO> toDtopclist (List<ParameterCategory> parameterCategories){
+        List<ParameterCategoryDTO> parameterCategoryDTO = new ArrayList<>();
+        for(ParameterCategory lista : parameterCategories) {
+            parameterCategoryDTO.add(this.toDtopc(lista));
+        }
+        return parameterCategoryDTO;
+    }
+
+    /**
+     * Transforms an object of type ParameterCategory into an object of type ParameterCategoryDto.
+     * @param parameterCategory An ParameterCategory object.
+     * @return An instance of ParameterCategoryDto.
+     */
+    public ParameterCategoryDTO toDtopc(ParameterCategory parameterCategory) {
+        return new ParameterCategoryDTO (parameterCategory.getCode(),parameterCategory.getName());
+    }
 }
--- src/test/java/app/mappers/ParameterMapperTest.java
+++ src/test/java/app/mappers/ParameterMapperTest.java
@@ -55,4 +55,48 @@ public class ParameterMapperTest {
 
         Assert.assertEquals(pcDto,parameterMapper.toDtoParameterCategoryDTo(pc));
     }
+
+    @Test
+    public void toModellista() {
+        ParameterMapper parameterMapper = new ParameterMapper();
+        ParameterCategory pc = new ParameterCategory("12A4D","Covid-19");
+        Parameter p = new Parameter("HB000","test","method", pc);
+
+
+        List<ParameterDTO> parameterDTOS = new ArrayList<>();
+        ParameterCategoryDTO pcDto = new ParameterCategoryDTO("12A4D","Covid-19");
+        ParameterDTO parameterDTO = new ParameterDTO("HB000","test","method", pcDto);
+        parameterDTOS.add(parameterDTO);
+
+        List<Parameter> parameterList = new ArrayList<>();
+        parameterList.add(p);
+
+
+        Assert.assertEquals(parameterList,parameterMapper.toModel(parameterDTOS));
+
+    }
+
+    @Test
+    public void ToModel() {
+        ParameterMapper parameterMapper = new ParameterMapper();
+        ParameterCategory pc = new ParameterCategory("12A4D","Covid-19");
+        ParameterCategoryDTO pcDto = new ParameterCategoryDTO("12A4D","Covid-19");
+        Parameter p = new Parameter("HB000","test","method", pc);
+
+        ParameterDTO parameterDTO = new ParameterDTO("HB000","test","method", pcDto);
+
+        Assert.assertEquals(p,parameterMapper.toModel(parameterDTO));
+
+
+    }
+
+    @Test
+    public void toModelParameterCategory() {
+        ParameterMapper parameterMapper = new ParameterMapper();
+        ParameterCategory pc = new ParameterCategory("12A4D","Covid-19");
+
+        ParameterCategoryDTO pcDto = new ParameterCategoryDTO("12A4D","Covid-19");
+
+        Assert.assertEquals(pc,parameterMapper.toModelParameterCategoryDTo(pcDto));
+    }
 }
