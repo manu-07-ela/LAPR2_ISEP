930da8c0f746e79b819d65f70bc51b16e74107ec
RitaAriana <1201386@isep.ipp.pt>
2021-06-02 22:58:38 +0100

    LoginUI and LabCoordinatorUI

--- src/main/java/app/ui/gui/LoginUi.java
+++ src/main/java/app/ui/gui/LoginUi.java
@@ -1,4 +1,52 @@
 package app.ui.gui;
 
+import app.controller.AuthController;
+import app.ui.console.MenuItem;
+import auth.mappers.dto.UserRoleDTO;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
 public class LoginUi {
+
+    private int tentativas = 3;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    /**
+     * Login.
+     */
+    @FXML
+    public void login() {
+        if (tentativas > 1) {
+            String emailInserido = txtEmail.getText().toLowerCase().trim();
+            String passwordInserida = txtPassword.getText();
+            AuthController ctrl = new AuthController();
+            boolean sucesso = ctrl.doLogin(emailInserido, passwordInserida);
+            if (sucesso) {
+
+            } else {
+                tentativas--;
+            }
+        } else {
+
+        }
+    }
+
+    /**
+     * redirects to ui method
+     *
+     * @param
+     */
+    private void redirectToRoleUI (List<MenuItem> rolesUI, UserRoleDTO role) {
+
+
+    }
+
 }
