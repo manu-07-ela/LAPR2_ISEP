efe936e4fc047b340cb7a70d40c4dccf03a59346
RitaAriana <1201386@isep.ipp.pt>
2021-06-05 12:36:48 +0100

    Lab Coordinator UI update

--- src/main/java/app/ui/gui/LabCoordinatorUi.java
+++ src/main/java/app/ui/gui/LabCoordinatorUi.java
@@ -1,17 +1,29 @@
 package app.ui.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 
 public class LabCoordinatorUi {
 
-    private Stage stage;
+    @FXML
+    private VBox ImportBotton;
+
+    @FXML
+    private VBox OverviewBotton;
 
     @FXML
     private BorderPane brdPane;
 
-    public void setLabelUI(Stage stageLabCoordinatorUI) {
-        this.stage = stageLabCoordinatorUI;
+    @FXML
+    void OverviewClickBotton(ActionEvent event) {
+
     }
+
+    @FXML
+    void ImportClickBotton(ActionEvent event) {
+
+    }
+
 }
