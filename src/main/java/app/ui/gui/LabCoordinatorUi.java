6b87c560aa21b3913f0ff14b25c0f79252c865dd
RitaAriana <1201386@isep.ipp.pt>
2021-06-05 11:50:15 +0100

    Login Ui updates

--- src/main/java/app/ui/gui/LabCoordinatorUi.java
+++ src/main/java/app/ui/gui/LabCoordinatorUi.java
@@ -1,11 +1,15 @@
 package app.ui.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class LabCoordinatorUi {
 
     private Stage stage;
 
+    @FXML
+    private BorderPane brdPane;
 
     public void setLabelUI(Stage stageLabCoordinatorUI) {
         this.stage = stageLabCoordinatorUI;
