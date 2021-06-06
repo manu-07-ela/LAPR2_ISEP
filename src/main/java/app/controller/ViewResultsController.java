97dad7b26132eb1ad6a40d4a760dbb947489fea6
JosePessoa007 <1201007@isep.ipp.pt>
2021-06-06 16:07:12 +0100

    US01 - Code update

--- src/main/java/app/controller/ViewResultsController.java
+++ src/main/java/app/controller/ViewResultsController.java
@@ -1,2 +1,64 @@
-package app.controller;public class ViewResultsController {
+package app.controller;
+
+import app.domain.model.Company;
+import app.domain.model.testrelated.Test;
+import app.domain.model.testrelated.TestType;
+import app.domain.model.users.Client;
+import app.domain.store.ClientStore;
+import app.domain.store.TestStore;
+import app.mappers.*;
+import app.mappers.dto.ClientDTO;
+import app.mappers.dto.TestDTO;
+import auth.domain.model.Email;
+
+import java.util.List;
+
+public class ViewResultsController {
+    /**
+     * Represents an instance of app.
+     */
+    private App app;
+    /**
+     * Represents a instance of company
+     */
+    private Company company;
+    /**
+     * Represents an instance of the test store.
+     */
+    private TestStore tStore;
+    /**
+     * Represents an instance of the test mapper.
+     */
+    private TestMapper tmapper;
+    /**
+     * Representes an instance of Client
+     */
+    private Client client;
+    /**
+     * Represents an instance of Client Store
+     */
+    private ClientStore clstore;
+    /**
+     * Represents an instance of Client Mapper
+     */
+    private ClientMapper clMapper;
+
+    public ViewResultsController() {
+        this(App.getInstance().getCompany());
+    }
+
+    public ViewResultsController(Company company) {
+        this.app=App.getInstance();
+        this.company=app.getCompany();
+        this.tStore=company.getTestStore();
+    }
+    public List<TestDTO> getTestList(ClientDTO cl){
+        List<Test> listTest = tStore.getClientTestsList(cl);
+        return tmapper.toDto(listTest);
+    }
+    public ClientDTO getUserSession(){
+        Email email = company.getAuthFacade().getCurrentUserSession().getEmail();
+        client = clstore.getClientByEmail(email.getEmail());
+        return clMapper.toDto(client);
+    }
 }
--- src/main/java/app/ui/console/functionalities/ViewResultsUI.java
+++ src/main/java/app/ui/console/functionalities/ViewResultsUI.java
@@ -1,12 +1,26 @@
 package app.ui.console.functionalities;
 
 
+import app.controller.ViewResultsController;
+import app.mappers.dto.ClientDTO;
+import app.ui.console.utils.Utils;
+
 public class ViewResultsUI implements Runnable{
 
+    /**
+     * Represents a instance of view results controller
+     */
+    private final ViewResultsController viewResultsctrl;
+
     public ViewResultsUI(){
+        viewResultsctrl = new ViewResultsController();
     }
 
     public void run() {
         System.out.printf("%nViewing Test Results%n");
+    }
+    public void viewResults(){
+        ClientDTO cl = viewResultsctrl.getUserSession();
+        Object option = Utils.showAndSelectOne(viewResultsctrl.getTestList(cl), "Choose what kind of Test Type should be registered");
     }
 }
