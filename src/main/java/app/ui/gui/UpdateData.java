06a8c82c8938eead3e6db3b85cf28fb5c080cbcc
Manuela Leite <1200720@isep.ipp.pt>
2021-06-07 12:39:36 +0100

    Updating the UpdateDataUI

--- src/main/java/app/ui/gui/UpdateData.java
+++ src/main/java/app/ui/gui/UpdateData.java
@@ -1,8 +1,120 @@
 package app.ui.gui;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.ContextMenuEvent;
+
+import java.awt.event.InputMethodEvent;
+import java.awt.event.MouseEvent;
 
 public class UpdateData {
+
+    @FXML
+    private TextField writeName;
+
+    @FXML
+    private Button SaveName;
+
+    @FXML
+    private TextField writeCitizenCard;
+
+    @FXML
+    private Button saveCitizenCard;
+
+    @FXML
+    private TextField writeNhsCode;
+
+    @FXML
+    private Button saveNhsCode;
+
+    @FXML
+    private TextField writeBirthDate;
+
+    @FXML
+    private Button saveBirthDate;
+
+    @FXML
+    private TextField writeSex;
+
+    @FXML
+    private Button saveSex;
+
+    @FXML
+    private TextField writeTin;
+
+    @FXML
+    private Button saveTin;
+
+    @FXML
+    private TextField writeEmail;
+
+    @FXML
+    private Button saveEmail;
+
+    @FXML
+    void saveBirthDateClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveCitizenCardClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveEmailClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveNameClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveNhsCodeClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveSexClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveTinClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void writeCitizenCardAction(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void writeEmailAction(ContextMenuEvent event) {
+
+    }
+
+    @FXML
+    void writeNameAction(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void writeNhsCodeAction(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void writeSexAction(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void writeTinAction(InputMethodEvent event) {
+
+    }
 
 }
