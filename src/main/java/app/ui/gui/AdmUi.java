b469185ca1999330b3a030565befec1345ab23e1
RitaAriana <1201386@isep.ipp.pt>
2021-06-08 19:26:15 +0100

    Logout

--- src/main/java/app/ui/gui/AdmUi.java
+++ src/main/java/app/ui/gui/AdmUi.java
@@ -1,7 +1,14 @@
 package app.ui.gui;
 
+import app.ui.console.AuthUI;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class AdmUi {
 
@@ -14,5 +21,10 @@ public class AdmUi {
     @FXML
     void closePlatform() {
         System.exit(0);
+    }
+
+    @FXML
+    void logout() {
+
     }
 }
