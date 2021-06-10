5cf107c7d9c6da10068c14f91ddb792dfd3358a0
Manuela Leite <1200720@isep.ipp.pt>
2021-06-10 20:25:20 +0100

    Updating the UI of the client

--- src/main/java/app/ui/gui/LoginUi.java
+++ src/main/java/app/ui/gui/LoginUi.java
@@ -160,7 +160,7 @@ public class LoginUi {
             stageClient = new Stage();
             stageClient.initStyle(StageStyle.UNDECORATED);
 
-            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("\\fxml\\ClientUi.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/ClientUi.fxml"));
             Parent root;
 
             root = loader.load();
