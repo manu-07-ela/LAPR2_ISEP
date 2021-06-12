fa4353c86f7933bb35bb2650cac46b39ec91df9c
RitaAriana <1201386@isep.ipp.pt>
2021-06-12 16:16:07 +0100

    Beginning of the implementation of US16

---
+++ src/main/java/app/adapter/interfaces/SubsequenceWithMaximumSum.java
@@ -0,0 +1,15 @@
+package app.adapter.interfaces;
+
+/**
+ * Interface that will be implemented by.
+ * @author Rita Ariana Sobral <1201386@isep.ipp.pt>
+ */
+
+public interface SubsequenceWithMaximumSum {
+
+    /**
+     * Method that will generate and return the subsequence with maximum sum.
+     * @return The subsequence with maximum sum.
+     */
+    int[] getSubsequenceWithMaximumSum (int[] seq);
+}
