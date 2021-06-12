ce74e6ed39652297cdc4dc3dd6066938e143d14c
Manuela Leite <1200720@isep.ipp.pt>
2021-06-12 19:38:20 +0100

    GUI of Specialist doctor

---
+++ src/main/java/app/ui/gui/SpecialistDoctorUi.java
@@ -0,0 +1,35 @@
+package app.ui.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class SpecialistDoctorUi {
+
+    @FXML
+    private VBox writeReport;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    void exitClick() {
+        System.exit(0);
+
+    }
+
+
+    @FXML
+    void logoutClick() {
+
+    }
+
+    @FXML
+    void writeReportClick() {
+
+    }
+
+}
