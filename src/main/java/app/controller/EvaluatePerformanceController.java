7851c3feb82ed849cb00e28b0579ea3a7bb182dc
RitaAriana <1201386@isep.ipp.pt>
2021-06-16 11:38:33 +0100

    US16 - UI Evaluete Performance

---
+++ src/main/java/app/controller/EvaluatePerformanceController.java
@@ -0,0 +1,50 @@
+package app.controller;
+
+import app.Serialization;
+import app.controller.App;
+import app.ui.console.AuthUI;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class EvaluatePerformanceController {
+
+    private Stage stage;
+
+    public void setLabelUI(Stage stage) {
+        this.stage = stage;
+    }
+
+    @FXML
+    void closePlatform() {
+        Serialization.saveApp(App.getInstance(), "SavedData.data");
+        System.exit(0);
+    }
+
+    @FXML
+    void logout() {
+        AuthUI uiLogin = new AuthUI();
+        uiLogin.logout();
+        try {
+            Stage newStage = new Stage();
+            newStage.initStyle(StageStyle.UNDECORATED);
+
+            Parent root;
+
+            root = FXMLLoader.load(getClass().getResource("/fxml/LoginUi.fxml"));
+
+            Scene scene = new Scene(root);
+
+            newStage.setScene(scene);
+            newStage.show();
+        } catch (IOException ex) {
+            System.out.println("Logout Error: " + ex);
+        }
+        stage.close();
+    }
+}
