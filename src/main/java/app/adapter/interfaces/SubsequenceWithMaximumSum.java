f3c7a0d3ab214fbb7e48f6ce0a1b5298db05498b
RitaAriana <1201386@isep.ipp.pt>
2021-06-16 09:53:08 +0100

    Resolving conflicts

--- src/main/java/app/adapter/interfaces/SubsequenceWithMaximumSum.java
+++ src/main/java/app/adapter/interfaces/SubsequenceWithMaximumSum.java
@@ -1,7 +1,7 @@
 package app.adapter.interfaces;
 
 /**
- * Interface that will be implemented by.
+ * Interface that will be implemented by the external module adapter responsible for obtaining the Subsequence With Maximum Sum.
  * @author Rita Ariana Sobral <1201386@isep.ipp.pt>
  */
 
@@ -11,5 +11,6 @@ public interface SubsequenceWithMaximumSum {
      * Method that will generate and return the subsequence with maximum sum.
      * @return The subsequence with maximum sum.
      */
+
     int[] getSubsequenceWithMaximumSum (int[] seq);
 }
