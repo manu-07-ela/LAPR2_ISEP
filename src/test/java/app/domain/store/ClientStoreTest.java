ac9a7c6896f84ac2ac36cd58746bb0c163f18331
pedroccelestino <1201382@isep.ipp.pt>
2021-06-16 10:08:15 +0100

    Resolving conflicts

--- src/test/java/app/domain/store/ClientStoreTest.java
+++ src/test/java/app/domain/store/ClientStoreTest.java
@@ -21,29 +21,37 @@ public class ClientStoreTest {
     ClientMapper clMapper;
     AuthFacade clauth;
 
-    @Before
-    public void setup() {
+    @Test
+    public void createClient() {
         clStore = new ClientStore();
         clMapper = new ClientMapper();
         clauth = new AuthFacade();
-        cl = new Client("Test","1234567891234567","1234567891","12/12/1995","Male","1234567891","12345678910","pessoa@gmail.com");
-    }
+        cl = new Client("Test","1234567892345678","1234567891","12/12/1995","Male","1234567890","12345678911","pessoa@gmail.com", "Rua da Paz");
 
-    @Test
-    public void createClient() {
-        ClientDTO clDTO = new ClientDTO("José Pessoa","1234567891234567","1234567891","12/12/1995","Male","1234567891","12345678910","pessoa@gmail.com");
+        ClientDTO clDTO = new ClientDTO("Test","1234567892345678","1234567891","12/12/1995","Male","1234567890","12345678911","pessoa@gmail.com", "Rua da Paz");
+
         Client result = clStore.createClient(clDTO,clMapper);
         Assert.assertEquals(cl, result);
     }
 
     @Test
     public void validateClient1() {
+        clStore = new ClientStore();
+        clMapper = new ClientMapper();
+        clauth = new AuthFacade();
+        cl = new Client("Test","1234567892345678","1234567891","12/12/1995","Male","1234567890","12345678911","pessoa@gmail.com", "Rua da Paz");
+
         boolean result = clStore.validateClient(cl);
         Assert.assertTrue(result);
     }
 
     @Test
     public void validateClient2() {
+        clStore = new ClientStore();
+        clMapper = new ClientMapper();
+        clauth = new AuthFacade();
+        cl = new Client("Test","1234567892345678","1234567891","12/12/1995","1234567890","12345678911","pessoa@gmail.com", "Rua da Paz");
+
         clStore.addClient(cl);
         boolean result = clStore.validateClient(cl);
         Assert.assertFalse(result);
@@ -51,6 +59,9 @@ public class ClientStoreTest {
 
     @Test
     public void validateClient3(){
+        clStore = new ClientStore();
+        clMapper = new ClientMapper();
+        clauth = new AuthFacade();
         Client cl = null;
         boolean result = clStore.validateClient(cl);
         Assert.assertFalse(result);
@@ -64,6 +75,11 @@ public class ClientStoreTest {
 
     @Test
     public void saveClient2() throws IOException {
+        clStore = new ClientStore();
+        clMapper = new ClientMapper();
+        clauth = new AuthFacade();
+        cl = new Client("Test","1234567892345678","1234567891","12/12/1995","Male","1234567890","12345678911","pessoa@gmail.com", "Rua da Paz");
+
         clStore.addClient(cl);
         boolean result = clStore.saveClient(cl,clauth);
         assertFalse(result);
@@ -71,8 +87,25 @@ public class ClientStoreTest {
 
     @Test
     public void saveClient3() throws IOException {
+        clStore = new ClientStore();
+        clMapper = new ClientMapper();
+        clauth = new AuthFacade();
+        cl = new Client("Test","1234567892345678","1234567891","12/12/1995","Male","1234567890","12345678911","pessoa@gmail.com", "Rua da Paz");
+
         Client cl = null;
         boolean result = clStore.saveClient(cl,clauth);
         Assert.assertFalse(result);
+    }
+
+    @Test
+    public void getClientByEmail(){
+        clStore = new ClientStore();
+        clMapper = new ClientMapper();
+        clauth = new AuthFacade();
+        cl = new Client("Test","1234567892345678","1234567891","12/12/1995","Male","1234567890","12345678911","pessoa@gmail.com", "Rua da Paz");
+
+        Client cl1 = clStore.getClientByEmail("pessoa@gmail.com");
+        Assert.assertTrue(cl,cl1);
+
     }
 }
