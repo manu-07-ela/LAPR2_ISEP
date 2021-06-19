85546c3a6bbe867867526729b5e34705947ad0fa
1201001 <1201001@isep.ipp.pt>
2021-06-19 21:42:03 +0100

    More Unitary tests

--- src/test/java/app/domain/model/testrelated/BarcodeDomainTest.java
+++ src/test/java/app/domain/model/testrelated/BarcodeDomainTest.java
@@ -1,5 +1,6 @@
 package app.domain.model.testrelated;
 
+import app.domain.model.attributes.Address;
 import app.domain.model.testrelated.BarcodeDomain;
 import net.sourceforge.barbecue.BarcodeException;
 import net.sourceforge.barbecue.BarcodeFactory;
@@ -7,40 +8,73 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BarcodeDomainTest {
-    //criar variaveis só dentro dos testes
-    String barcodeNumber = "00000000000";
-    BarcodeDomain barcodeDomain;
-    BarcodeDomain barcodeDomainAux;
+
 
     @Test(expected = IllegalArgumentException.class)
     public void barcodeNotNull(){
-        barcodeDomain = new BarcodeDomain(null, null);
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(null, null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void setBarcodeNumberNotNull() throws BarcodeException {
-        barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA("00000000000"), null);
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA("00000000000"), null);
     }
 
     @Test
     public void getBarcode() throws BarcodeException {
-        barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
-        barcodeDomainAux = barcodeDomain;
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
+        BarcodeDomain barcodeDomainAux = barcodeDomain;
         Assert.assertEquals(barcodeDomain, barcodeDomainAux);
     }
 
     @Test
     public void getBarcodeNumber() throws BarcodeException {
-        barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
         String result = "00000000000";
         Assert.assertEquals(barcodeDomain.getBarcodeNumber(), result);
     }
 
     @Test
     public void testEquals() throws BarcodeException {
-        barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
-        barcodeDomainAux = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
+        BarcodeDomain barcodeDomainAux = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
         Assert.assertNotEquals(barcodeDomain, barcodeDomainAux);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void BarcodeDomainValidation() throws BarcodeException {
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(null);
+    }
+
+    @Test
+    public void BarcodeDomainValidation2() throws BarcodeException {
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
+        BarcodeDomain barcodeDomainAux = new BarcodeDomain(barcodeDomain);
+    }
+
+    @Test
+    public void BarcodeDomainEqualsOtherClass() throws BarcodeException {
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
+        Address name2 = new Address("Rua das cavalas");
+        Assert.assertFalse(barcodeDomain.equals(name2));
+    }
+
+    @Test
+    public void BarcodeDomainEquals() throws BarcodeException {
+        String barcodeNumber = "00000000000";
+        BarcodeDomain barcodeDomain = new BarcodeDomain(BarcodeFactory.createUPCA(barcodeNumber), barcodeNumber);
+        BarcodeDomain barcodeDomainAux = new BarcodeDomain(barcodeDomain);
+        Assert.assertTrue(barcodeDomain.equals(barcodeDomainAux));
+    }
+
+
 
 }
