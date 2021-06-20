4a94a775274abdce099e697faf64293de7cefa77
1201001 <1201001@isep.ipp.pt>
2021-06-20 11:41:09 +0100

    Creation of more Unitary Tests

--- src/test/java/app/domain/model/users/SpecialistDoctorTest.java
+++ src/test/java/app/domain/model/users/SpecialistDoctorTest.java
@@ -10,13 +10,6 @@ import org.junit.Test;
 
 public class SpecialistDoctorTest {
 
-    SpecialistDoctor spDoc;
-
-    @Before
-    public void setup(){
-        spDoc = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
-    }
-
     @Test(expected = NullPointerException.class)
     public void ensureSpecialistDoctorNullIsNotAllow(){
         new SpecialistDoctor(null, null, null, null, null, null, null);
@@ -24,6 +17,7 @@ public class SpecialistDoctorTest {
 
     @Test
     public void getDoctorIndexNumber() {
+        SpecialistDoctor spDoc = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
         DoctorIndexNumber expectedResult = new DoctorIndexNumber("123456");
         DoctorIndexNumber result = spDoc.getDoctorIndexNumber();
         Assert.assertEquals(expectedResult, result);
@@ -31,12 +25,47 @@ public class SpecialistDoctorTest {
 
     @Test
     public void testEquals() {
-        SpecialistDoctor spDoc1 = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123126"));
-        SpecialistDoctor spDoc2 = spDoc1;
-
-        Assert.assertEquals(spDoc1, spDoc2);
-        Assert.assertNotEquals(spDoc1, spDoc);
+        SpecialistDoctor spDoc = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
+        SpecialistDoctor spDoc1 = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
 
 
+        Assert.assertTrue(spDoc.equals(spDoc1));
     }
+
+    @Test
+    public void testEqualsReference() {
+        SpecialistDoctor spDoc = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
+        SpecialistDoctor spDoc1 = spDoc;
+
+
+        Assert.assertTrue(spDoc.equals(spDoc1));
+    }
+
+    @Test
+    public void testEqualsNull() {
+        SpecialistDoctor spDoc = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
+        SpecialistDoctor spDoc1 = null;
+
+
+        Assert.assertFalse(spDoc.equals(spDoc1));
+    }
+
+    @Test
+    public void testEqualsOtherClass() {
+        SpecialistDoctor spDoc = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
+        Address name2 = new Address("Rua das cavalas");
+
+
+        Assert.assertFalse(spDoc.equals(name2));
+    }
+
+    @Test
+    public void testEquals2() {
+        SpecialistDoctor spDoc = new SpecialistDoctor(new Name("Manuela de Araujo Leite"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123456"));
+        SpecialistDoctor spDoc1 = new SpecialistDoctor(new Name("Manuela de Ara"), new Email("1200720@isep.ipp.pt"), new Address("Rua 15"),new PhoneNumber("12338725367"),new SocCode("1234"), new OrganizationRole("Specialist Doctor"), new DoctorIndexNumber("123126"));
+
+
+        Assert.assertFalse(spDoc.equals(spDoc1));
+    }
+
 }
